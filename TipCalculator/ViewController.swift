aee559edeb548427066c4c881d505f6df5751734
jonzamora18 <jonzamora18@gmail.com>
2020-08-02 18:18:07 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Jonathan Zamora on 8/2/20.
+//  Copyright © 2020 Jonathan Zamora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
